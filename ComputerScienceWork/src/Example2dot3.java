2a46650935c9a9c9c12e162df0ba15942e275299
Nathan <NJohnson.2017@littletonps.org>
2014-09-28 20:00:38 -0400

    New Work

---
+++ ComputerScienceWork/src/Example2dot3.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Example2dot3 {
+	public static void main(String...args) {
+		Scanner reader = new Scanner(System.in);
+		
+		double kilometers;
+		double nautical;		
+		System.out.println("Enter distance in kilometers: ");
+		kilometers = reader.nextDouble();
+		
+		nautical = ((60.0 * 90.0)/10000) * kilometers;
+		
+		System.out.println("There are " + nautical + " nautical miles in " + kilometers + " kilometers");
+	}
+}
