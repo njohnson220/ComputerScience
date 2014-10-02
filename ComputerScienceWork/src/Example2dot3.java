778fbb96afe4b55c240c5e9551fa1edf707282c9
njohnson220 <NJohnson.2017@littletonps.org>
2014-10-02 09:26:20 -0400

    testing nextdouble

--- ComputerScienceWork/src/Example2dot3.java
+++ ComputerScienceWork/src/Example2dot3.java
@@ -7,10 +7,10 @@ public class Example2dot3 {
 		double kilometers;
 		double nautical;		
 		System.out.println("Enter distance in kilometers: ");
-		kilometers = reader.nextDouble();
+		kilometers = (int)reader.nextDouble();
 		
 		nautical = ((60.0 * 90.0)/10000) * kilometers;
-		
+		System.out.println(kilometers);
 		System.out.println("There are " + nautical + " nautical miles in " + kilometers + " kilometers");
 	}
 }
