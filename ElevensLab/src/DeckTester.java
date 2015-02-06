6eac0e4333b25cf2455b734b12e9e973587734aa
VHS <VHS@10.0.36.27>
2015-02-06 14:28:41 -0500

    implemented deck tester

---
+++ ElevensLab/src/DeckTester.java
@@ -0,0 +1,24 @@
+/**
+ * This is a class that tests the Deck class.
+ */
+public class DeckTester {
+
+	/**
+	 * The main method in this class checks the Deck operations for consistency.
+	 *	@param args is not used.
+	 */
+	public static void main(String[] args) {
+		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+		
+		String[] ranks = {"A", "B", "C"};
+		String[] suits = {"Giraffes", "Lions"};
+		int[] values = {2, 1, 6};
+		
+		Deck deck1 = new Deck(ranks, suits, values);
+		
+		System.out.println(deck1.isEmpty());
+		Card myCard = deck1.deal();
+		System.out.println(deck1.toString());
+
+	}
+}
