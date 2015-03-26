e05b0ece1d52a42c163bfc93e955e2f61e961476
Nathan <NJohnson.2017@littletonps.org>
2015-03-25 20:47:06 -0400

    finished 26.4 Program It

--- 26.4 Program It/src/Student.java
+++ 26.4 Program It/src/Student.java
@@ -2,81 +2,107 @@ import java.util.*;
 
 
 public class Student {
-	
-	private String id;
-	private String [] quizresult=new String[10]; 
-	private int score;
-	
-	public Student(){
-		
-		id=" ";
-		for(int i =0; i<quizresult.length; i++){
-		     quizresult[i]="";
-		     			
-		}
-		
-		score =0;
-	}
-	
-	
-	public Student(String a, String[] answers){
-		
-		id=a;
-		
-		for(int i=0; i<answers.length; i++){
-			
-			quizresult[i]=answers[i];
-			
-		}
-		
-		score = 0;
-		
-	}
-	
-	public String [] getAnwers(){
-		
-		return quizresult;
-		
-	}
-	
-	public String getId(){
-		
-		return id;
-		
-	}
-	
-	
-	public void grade(String [] answerkey, String[] studentanswer){
-		
-		int count=0;
-		
-				for(int i=0; i<answerkey.length; i++){
-					
-					if(answerkey[i].equals(studentanswer[i])){
-						
-						count = count+10;
-						
-					}
-					
-				}
-		
-				score = count;
-		
-		
-	}
-	
-	public String toString(){
-		
-		String a = "Student id: " + id + " " +  Arrays.toString(quizresult) + " The score for this quiz is:  " +  score + "\n";
-		
-		return a;
-		
-		
-			
-			}
+
+    private String id;
+    private String[] quizresult = new String[10];
+    private int score;
+    private String letterGrade;
+
+    public Student() {
+
+        id = " ";
+        for (int i = 0; i < quizresult.length; i++) {
+            quizresult[i] = "";
+        }
+
+        score = 0;
+    }
+
+    public Student(String a, String[] answers) {
+
+        id = a;
+
+        for (int i = 0; i < answers.length; i++) {
+
+            quizresult[i] = answers[i];
+
+        }
+
+        score = 0;
+
+    }
+
+    public String[] getAnwers() {
+        return quizresult;
+
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public String getId() {
+        return id;
+
+    }
+
+    public String getLetterGrade() { //returns the letter grade of said student
+        return letterGrade;
+    }
+
+
+    public void grade(String[] answerkey, String[] studentanswer) {
+
+        int count = 0;
+
+        for (int i = 0; i < answerkey.length; i++) {
+
+            if (answerkey[i].equals(studentanswer[i])) {
+
+                count = count + 10;
+
+            }
+
+        }
+
+        score = count;
+
+
+    }
+
+    private String setLetterGrade() { //this method gets a letter grade based on his/her number score
+        int numberGrade = score;
+        if (score >= 90) {
+            letterGrade = "A";
+        }
+        else if (score >= 80 && score < 90) {
+            letterGrade = "B";
+        }
+        else if (score >= 70 && score < 80) {
+            letterGrade = "C";
+        }
+        else if (score >= 60 && score < 70) {
+            letterGrade = "D";
+        }
+        else if (score < 60) {
+            letterGrade = "F";
+        }
+
+        return letterGrade;
+    }
+
+    public String toString() {
+
+        String a = "Student id: " + id + " " + Arrays.toString(quizresult) + " The score for this quiz is:  " + score
+                    + " The letter grade for this quiz is: " + setLetterGrade() + "\n";
+
+        return a;
+
+
+    }
 
 }
-	
+
 	
 
 
--- 26.4 Program It/src/Test.java
+++ 26.4 Program It/src/Test.java
@@ -1,3 +1,10 @@
+/*
+Nathan Johnson
+3/25/15
+Added sorting mechanism to the Test class, sorting students in order from lowest to highest grade.
+Specifically, I used the selection sort model from the textbook by adapting it to this program's needs.
+ */
+
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -6,104 +13,120 @@ import java.util.StringTokenizer;
 import java.util.*;
 
 
-
 public class Test {
-    //test
-
-	
-	      private static FileInputStream inFile;
-		  private static InputStreamReader inReader;
-		  private static  BufferedReader reader;
-
-		  
-		 private static List<Student> classroom = new ArrayList<Student>(); // ArrayList to store the classroom.
-		 
-		  
-		  
-		  
-		  
-
-			  public static void main (String args[]) throws IOException
-			  {
-			    initFile();  
-			    getData();
-			    System.out.print(classroom);  //output of the complete class.
-			    sort();
-			    System.out.print(classroom); //output after sorting.
-			    inFile.close();
-			    
-			  }
-			 
-			 // preparing the file for input
-			 
-			  public static void initFile() throws IOException
-
-			  
-			  {
-			    inFile = new FileInputStream ("c:\\!!VHSAPCS\\truefalse.txt");
-			    inReader = new InputStreamReader(inFile);
-			    reader = new BufferedReader(inReader);
-			    
-			  }
-	
-			  
-			  //  Separate the id from the answers and store the answers in an array.
-			  
-			  public static void getData() throws IOException
-			  {
-			    String line = reader.readLine();  //Seed
-			    
-			    String[] answerkey = new String[10];  //Store the answer key from the first line of the txt file.
-			    
-			    for(int i=0; i<answerkey.length; i++){ // take that line and place each answer in an array.
-			    	
-			    	answerkey[i]=line.substring(i,i+1);			    	
-			    }
-			    
-			    line = reader.readLine();    // read the following line of the txt file.
-			    			     
-			                                
-			    while(line != null)   // Read and create a student for each line.
-			    {
-			      String[] answers = new String[10];
-			      StringTokenizer strTkn = new StringTokenizer(line); 
-			      String id = strTkn.nextToken();
-			      String answerline = strTkn.nextToken();
-			      
-						      
-			      for(int i=0; i<answers.length; i++){
-			    	  
-			    	  answers[i]=answerline.substring(i, i+1);
-			    	  
-			      }
-			      
-			      Student stu = new Student(id,answers);
-			      
-			      stu.grade(answerkey, answers);
-			      
-			      classroom.add(stu);  
-			     
-			      
-			      
-			      			      
-			      line = reader.readLine();   //updating what is being read
-			                                  
-			    }
-			    
-			  }
-			  
-			
-			  // In this method you should sort the classroom in ascending order depending on the score obtained on the quiz. 
-			  public static void sort(){
-				  
-				  //Implementation of code here.
-				  
-				  
-			  }
 
 
-				  
-	  }
+    private static FileInputStream inFile;
+    private static InputStreamReader inReader;
+    private static BufferedReader reader;
+
+
+    private static List<Student> classroom = new ArrayList<Student>(); // ArrayList to store the classroom.
+
+    public static void main(String args[]) throws IOException {
+        initFile();
+        getData();
+        System.out.print(classroom + "\n\n\n");  //output of the complete class.
+        selectionSort();
+        System.out.print(classroom); //output after sorting.
+        inFile.close();
+
+    }
+
+    public static int findLowestGrade(List<Student> unsortedList, int first) { //finds the lowest grade of the class in order to start the selection sort
+        int minIndex = first;
+        for (int i = first + 1; i < unsortedList.size(); i++) {
+            if ((unsortedList.get(i).getScore()) < unsortedList.get(minIndex).getScore()) {
+                minIndex = i;
+            }
+        }
+        return minIndex;
+    }
+
+    public static void swap(List<Student> swappingList, int x, int y) { //swaps two values of the student arraylist in order to facilitate sorting
+        Student temporary = swappingList.get(x);
+        swappingList.set(x, swappingList.get(y));
+        swappingList.set(y, temporary);
+    }
+
+    public static void selectionSort() {  //selection sorts the student arraylist
+        List<Student> sortedByGrade = classroom;
+        for (int i = 0; i < sortedByGrade.size() - 1; i++) {
+            int minIndex = findLowestGrade(sortedByGrade, i);
+            if (minIndex != i) {
+                swap(sortedByGrade, i, minIndex);
+            }
+
+        }
+        classroom = sortedByGrade;
+    }
+
+    // preparing the file for input
+
+    public static void initFile() throws IOException
+
+
+    {
+        inFile = new FileInputStream("c:\\!!VHSAPCS\\truefalse.txt");
+        inReader = new InputStreamReader(inFile);
+        reader = new BufferedReader(inReader);
+
+    }
+
+
+    //  Separate the id from the answers and store the answers in an array.
+
+    public static void getData() throws IOException {
+        String line = reader.readLine();  //Seed
+
+        String[] answerkey = new String[10];  //Store the answer key from the first line of the txt file.
+
+        for (int i = 0; i < answerkey.length; i++) { // take that line and place each answer in an array.
+
+            answerkey[i] = line.substring(i, i + 1);
+        }
+
+        line = reader.readLine();    // read the following line of the txt file.
+
+
+        while (line != null)   // Read and create a student for each line.
+        {
+            String[] answers = new String[10];
+            StringTokenizer strTkn = new StringTokenizer(line);
+            String id = strTkn.nextToken();
+            String answerline = strTkn.nextToken();
+
+
+            for (int i = 0; i < answers.length; i++) {
+
+                answers[i] = answerline.substring(i, i + 1);
+
+            }
+
+            Student stu = new Student(id, answers);
+
+            stu.grade(answerkey, answers);
+
+            classroom.add(stu);
+
+
+            line = reader.readLine();   //updating what is being read
+
+        }
+
+    }
+
+
+    // In this method you should sort the classroom in ascending order depending on the score obtained on the quiz.
+    public static void sort() {
+
+        //Implementation of code here.
+
+
+    }
+
+
+}
 
 
 
